41c7b32c631d7f9ddc59b669aff8a94859ffa841
Robert van den Boorn <robertb@uniform.dev>
2024-06-07 12:23:41 +0200

    restored hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,35 @@
-import {  ComponentProps, UniformRichText, UniformText, registerUniformComponent } from "@uniformdev/canvas-react";
-
-type Asset = {
-  fields: {
-    url: {
-        value: string;
-    }
-  }
-};
+import { UniformRichText } from "@uniformdev/canvas-next";
+import {
+  registerUniformComponent,
+  ComponentProps,
+  UniformText,
+} from "@uniformdev/canvas-react";
 
 type HeroProps = ComponentProps<{
   title: string;
   description?: string;
-  image?: Asset[];
 }>;
 
-const Hero: React.FC<HeroProps> = ({ title, description, image }) => (
-   <div className="feature-container">
-  {image && image.length > 0 && (
-    <img src={image[0].fields.url.value} alt="Feature image" className="feature-image" />
-  )}
-  <div className="text-container">
+const Hero: React.FC<HeroProps> = () => (
+  <div>
     <UniformText
       className="title"
       parameterId="title"
       as="h1"
-      data-test-id="feature-title"
-      placeholder="Feature title goes here"
+      data-test-id="hero-title"
+      placeholder="Hero title goes here"
     />
     <UniformRichText
       parameterId="description"
       className="description"
-      data-test-id="feature-description"
+      data-test-id="hero-description"
     />
   </div>
-</div>
 );
 
-export default Hero;
+registerUniformComponent({
+  type: "hero",
+  component: Hero,
+});
 
-registerUniformComponent({ type: "hero", component: Hero })
+export default Hero;
